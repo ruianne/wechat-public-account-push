f2c5ac999e599d94f691bc7037daf437d73a58ab
ruianne <117987534+ruianne@users.noreply.github.com>
2022-11-11 20:15:42 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -54,7 +54,7 @@ const USER_CONFIG = {
 
   CALLBACK_USERS: [
     {
-      name: 'YLC',
+      name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oQ1MU6axbg6XQWG4MMVIuopNalaI',
     }
